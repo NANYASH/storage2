09383bfa11c6e8492a74d04b6602e09c26db341f
nanyash <miss.nanya80@gmail.com>
2018-09-03 12:37:30 +0300

    initial

--- src/main/java/com/dao/FileDAO.java
+++ src/main/java/com/dao/FileDAO.java
@@ -11,25 +11,55 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class FileDAO extends GenericDAO<File> {
-    private static final String SAVE = "INSERT INTO С_FILE(ID,FILE_NAME,FILE_FORMAT,FILE_SIZE,ID_STORAGE) " +
-            "VALUES(FILE_SEQ.NEXTVAL,?,?,?, (SELECT ID FROM (SELECT ID " +
-            "FROM STORAGE " +
-            "WHERE ID = ? AND " +
-            "FORMATS_SUPPORTED = ? AND " +
-            "STORAGE_SIZE - COALESCE((SELECT SUM(FILE_SIZE) FROM С_FILE WHERE ID_STORAGE = ? ),0) > ?)WHERE ROWNUM = 1))";
-    private static final String DELETE_FILE = "DELETE FROM С_FILE WHERE ID=?";
-    private static final String VALIDATE_FILE = "SELECT ID FROM STORAGE " +
+     private static final String VALIDATE_STORAGE = "SELECT ID FROM STORAGE " +
             "WHERE ID = ? AND " +
             "FORMATS_SUPPORTED = (SELECT FILE_FORMAT FROM С_FILE WHERE ID = ?) AND " +
             "(STORAGE_SIZE - COALESCE((SELECT SUM(FILE_SIZE) FROM С_FILE WHERE ID_STORAGE = ?),0)) > (SELECT FILE_SIZE FROM С_FILE WHERE ID = ?)";
     private static final String TRANSFER_FILE = "UPDATE С_FILE SET ID_STORAGE = ? " +
             "WHERE ID_STORAGE = ? AND ID = ?";
 
+    private static final String VALIDATE_FILE = "SELECT ID " +
+            "FROM STORAGE " +
+            "WHERE ID = ? AND " +
+            "FORMATS_SUPPORTED = ? AND " +
+            "STORAGE_SIZE - COALESCE((SELECT SUM(FILE_SIZE) FROM С_FILE WHERE ID_STORAGE = ? ),0) > ?";
+
+    private static final String PUT_FILE = "INSERT INTO С_FILE(ID,FILE_NAME,FILE_FORMAT,FILE_SIZE,ID_STORAGE) " +
+            "VALUES(FILE_SEQ.NEXTVAL,?,?,?,?)";
+
+    private static final String DELETE_FILE = "DELETE FROM С_FILE WHERE ID=?";
+
+    @Override
+    public File save(File file) throws InternalServerError {
+        Transaction tr = null;
+        try(Session session = createSessionFactory().openSession()) {
+            NativeQuery validateQuery = session.createNativeQuery(VALIDATE_FILE);
+            NativeQuery insertQuery = session.createNativeQuery(PUT_FILE);
+            validateQuery.setParameter(1,file.getStorage().getId());
+            validateQuery.setParameter(2,file.getFormat());
+            validateQuery.setParameter(3,file.getStorage().getId());
+            validateQuery.setParameter(4,file.getSize());
+            insertQuery.setParameter(1,file.getName());
+            insertQuery.setParameter(2,file.getFormat());
+            insertQuery.setParameter(3,file.getSize());
+            insertQuery.setParameter(4,file.getStorage().getId());
+            tr = session.getTransaction();
+            tr.begin();
+            if (validateQuery.getSingleResult()!=null)
+                insertQuery.executeUpdate();
+            tr.commit();
+            return file;
+        }catch (HibernateException e){
+            System.err.println(e.getMessage());
+            throw new InternalServerError("Internal Server Error");
+        }
+    }
+
     public int transferFile(long from,long to, long fileId) throws InternalServerError, BadRequestException {
         Transaction tr = null;
         int result = 0;
         try (Session session = createSessionFactory().openSession()) {
-            NativeQuery validateQuery = session.createNativeQuery(VALIDATE_FILE);
+            NativeQuery validateQuery = session.createNativeQuery(VALIDATE_STORAGE);
             NativeQuery updateQuery = session.createNativeQuery(TRANSFER_FILE);
             validateQuery.setParameter(1, to);
             validateQuery.setParameter(2, fileId);
@@ -45,30 +75,6 @@ public class FileDAO extends GenericDAO<File> {
             tr.commit();
             return result;
         } catch (HibernateException e) {
-            System.err.println(e.getMessage());
-            throw new InternalServerError("Internal Server Error");
-        }
-    }
-
-    @Override
-    public File save(File file) throws InternalServerError {
-        Transaction tr = null;
-        try(Session session = createSessionFactory().openSession()) {
-            NativeQuery query = session.createNativeQuery(SAVE);
-            query.addEntity(File.class);
-            query.setParameter(1,file.getName());
-            query.setParameter(2,file.getFormat());
-            query.setParameter(3,file.getSize());
-            query.setParameter(4,file.getStorage().getId());
-            query.setParameter(5,file.getFormat());
-            query.setParameter(6,file.getStorage().getId());
-            query.setParameter(7,file.getSize());
-            tr = session.getTransaction();
-            tr.begin();
-            query.executeUpdate();
-            tr.commit();
-            return file;
-        }catch (HibernateException e){
             System.err.println(e.getMessage());
             throw new InternalServerError("Internal Server Error");
         }
